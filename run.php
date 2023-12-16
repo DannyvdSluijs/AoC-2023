5850140c8cb5984df0ce4fe8d95f35ff8ec9b8c9
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2023-12-16 22:15:45 +0100

    fix: Small correction to the help output of the run command

--- run.php
+++ run.php
@@ -9,7 +9,7 @@ require_once 'vendor/autoload.php';
 ini_set('memory_limit','2048M');
 
 if ($argc < 2 || $argc > 3 || !is_numeric($argv[1])) {
-    print("Usage ./run.php <day> [part]\r\nPossible values for part are: 'input', 1 or 2");
+    print("Usage ./run.php <day> [part]\r\nPossible values for part are: 'input', 1 or 2\r\n");
     exit(255);
 }
 
