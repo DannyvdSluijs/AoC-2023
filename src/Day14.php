a1a6e474cb77cd4b7e68d62e3a874f88bf1b5b3d
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2024-01-02 23:35:21 +0100

    feat: Day 14 part 2 solution

--- src/Day14.php
+++ src/Day14.php
@@ -12,40 +12,201 @@ class Day14
 
     public function partOne(): string
     {
-        $grid = $this->readInputAsGridOfCharacters();
-        $depth = count($grid);
-        $width = count($grid[0]);
-
-        $columns = [];
-        $results = [];
-        $scores = [];
-        for ($x = 0; $x < $width; $x++) {
-            $colum = array_column($grid, $x);
-            $chunks = explode('#', implode($colum));
-            $result = '';
-
-            $columnScore = 0;
-            $pointer = $depth;
-            foreach ($chunks as $chunk) {
-                for($o = 0, $maxO = substr_count($chunk, 'O'); $o < $maxO; $o++) {
-                    $columnScore += $pointer;
-                    $pointer--;
-                }
-                for($dot = 0, $maxDot = substr_count($chunk, '.'); $dot < $maxDot; $dot++) {
-                    $pointer--;
-                }
-                $pointer--;
-            }
-            $scores[] = $columnScore;
-         }
-
-        return (string) array_sum($scores);
+        $rows = $this->readInputAsGridOfCharacters();
+        $rows = $this->tiltNorth($rows);
+        return (string) $this->computeLoadOfNorthSupportBeam($rows);
     }
 
     public function partTwo(): string
     {
-        $content = $this->readInput();
+        $rows = $this->readInputAsGridOfCharacters();
+        $cycles = 1_000_000_000;
+        $results = [];
 
-        return '';
+        for ($cycle = 0; $cycle < $cycles; $cycle++) {
+            $rows = $this->tiltNorth($rows);
+            $rows = $this->tiltWest($rows);
+            $rows = $this->tiltSouth($rows);
+            $rows = $this->tiltEast($rows);
+
+            if (in_array($rows, $results, true)) {
+                $match = array_search($rows, $results, true);
+                $loopSize = $cycle - $match;
+                $remainingCycles = $cycles - $cycle;
+                $reduced = $remainingCycles % $loopSize;
+                $cycles = $cycle + $reduced;
+            }
+            $results[] = $rows;
+        }
+
+        return (string) $this->computeLoadOfNorthSupportBeam($rows);
+    }
+
+    private function computeLoadOfNorthSupportBeam(array $grid): int
+    {
+        $depth = count($grid);
+
+
+        $score = 0;
+        foreach ($grid as $index => $row) {
+            $count = substr_count(implode($row), 'O');
+            $score += ($depth - $index) * $count;
+        }
+
+        return $score;
+    }
+
+    private function tiltNorth(array $rows): array
+    {
+        $depth = count($rows);
+        $width = count($rows[0]);
+
+        // Rows to columns for tilt to north
+        $columns = [];
+        for ($x = 0; $x < $width; $x++) {
+            $columns[] = array_column($rows, $x);
+        }
+        for ($x = 0; $x < $width; $x++) {
+            $oCount = 0;
+            $dotCount = 0;
+            $result = [];
+            for ($y = 0; $y < $depth; $y++) {
+                if ($columns[$x][$y] === '.') {
+                    $dotCount++;
+                    continue;
+                }
+                if ($columns[$x][$y] === 'O') {
+                    $oCount++;
+                    continue;
+                }
+
+                $result[] = array_fill(0, $oCount, 'O');
+                $result[] = array_fill(0, $dotCount, '.');
+                $result[] = ['#'];
+                $oCount = 0;
+                $dotCount = 0;
+            }
+            $result[] = array_fill(0, $oCount, 'O');
+            $result[] = array_fill(0, $dotCount, '.');
+
+            $columns[$x] = array_merge(...$result);
+        }
+
+        // Columns to rows for tilt to west
+        $rows = [];
+        for ($x = 0; $x < $depth; $x++) {
+            $rows[] = array_column($columns, $x);
+        }
+
+        return $rows;
+    }
+
+    private function tiltWest(array $rows): array
+    {
+        $width = count($rows[0]);
+
+        foreach ($rows as $x => $xValue) {
+            $oCount = 0;
+            $dotCount = 0;
+            $result = [];
+            for ($y = 0; $y < $width; $y++) {
+                if ($xValue[$y] === '.') {
+                    $dotCount++;
+                    continue;
+                }
+                if ($xValue[$y] === 'O') {
+                    $oCount++;
+                    continue;
+                }
+
+                $result[] = array_fill(0, $oCount, 'O');
+                $result[] = array_fill(0, $dotCount, '.');
+                $result[] = ['#'];
+                $oCount = 0;
+                $dotCount = 0;
+            }
+            $result[] = array_fill(0, $oCount, 'O');
+            $result[] = array_fill(0, $dotCount, '.');
+
+            $rows[$x] = array_merge(...$result);
+        }
+
+        return $rows;
+    }
+
+    private function tiltSouth(array $rows): array
+    {
+        $depth = count($rows);
+        $width = count($rows[0]);
+        // Rows to columns for tilt to south
+        $columns = [];
+        for ($x = 0; $x < $width; $x++) {
+            $columns[] = array_column($rows, $x);
+        }
+        for ($x = 0; $x < $width; $x++) {
+            $oCount = 0;
+            $dotCount = 0;
+            $result = [];
+            for ($y = $depth - 1; $y >= 0; $y--) {
+                if ($columns[$x][$y] === '.') {
+                    $dotCount++;
+                    continue;
+                }
+                if ($columns[$x][$y] === 'O') {
+                    $oCount++;
+                    continue;
+                }
+
+                $result[] = array_fill(0, $oCount, 'O');
+                $result[] = array_fill(0, $dotCount, '.');
+                $result[] = ['#'];
+                $oCount = 0;
+                $dotCount = 0;
+            }
+            $result[] = array_fill(0, $oCount, 'O');
+            $result[] = array_fill(0, $dotCount, '.');
+
+            $columns[$x] = array_reverse(array_merge(...$result));
+        }
+
+        // Columns to rows for tilt to east
+        $rows = [];
+        for ($x = 0; $x < $depth; $x++) {
+            $rows[] = array_column($columns, $x);
+        }
+
+        return $rows;
+    }
+
+    private function tiltEast(array $rows): array
+    {
+        $width = count($rows[0]);
+        foreach ($rows as $x => $xValue) {
+            $oCount = 0;
+            $dotCount = 0;
+            $result = [];
+            for ($y = $width - 1; $y >= 0; $y--) {
+                if ($xValue[$y] === '.') {
+                    $dotCount++;
+                    continue;
+                }
+                if ($xValue[$y] === 'O') {
+                    $oCount++;
+                    continue;
+                }
+
+                $result[] = array_fill(0, $oCount, 'O');
+                $result[] = array_fill(0, $dotCount, '.');
+                $result[] = ['#'];
+                $oCount = 0;
+                $dotCount = 0;
+            }
+            $result[] = array_fill(0, $oCount, 'O');
+            $result[] = array_fill(0, $dotCount, '.');
+
+            $rows[$x] = array_reverse(array_merge(...$result));
+        }
+
+        return $rows;
     }
 }
